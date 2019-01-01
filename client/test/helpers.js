4f357128b4295ce4e7d41a0c02f0e83d9f0e6351
travor <travor20814@gmail.com>
2019-01-01 22:23:57 +0800

    remove unuse helper

--- client/test/helpers.js
+++ client/test/helpers.js
@@ -1,7 +1,5 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 import {
-  mount,
-  render,
   configure,
 } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -9,7 +7,6 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 // global.expect = expect;
-
-global.mount = mount;
-global.render = render;
+// global.mount = mount;
+// global.render = render;
 // global.shallow = shallow;
